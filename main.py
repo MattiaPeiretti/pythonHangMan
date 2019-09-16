6e4fe9e1a4fd92b1b1458e14478abb9a44f5c5ef
Mattia Peiretti <mattia.peiretti@gmail.com>
2019-09-16 20:57:30 +0200

    version 0.2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from nouns import nouns, compliments
 MIN_ASCII_VALUE = 97
 MAX_ASCII_VALUE = 122
 
-WINDOW_WIDTH = 1000
+WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
 
 BACKGROUND_COLOR = (0, 0, 0)
@@ -20,6 +20,7 @@ DELAY_COSTANT = 500
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 24)
 myfont_big = pygame.font.SysFont('Comic Sans', 54)
+myfont_small = pygame.font.SysFont('sans serif', 20)
 
 class stick_man:
     def __init__(self, screen, color):
@@ -44,7 +45,6 @@ class stick_man:
     def draw_right_leg(self):
         pygame.draw.line(self.screen, self.color, (200, 160), (170, 200))
 
-
 def sleep(sec):
     for _ in range(0, sec*DELAY_COSTANT):
         for events in pygame.event.get():
@@ -52,107 +52,33 @@ def sleep(sec):
                 sys.exit()
         time.sleep(0.001)
 
-
 def draw_base(screen, FOREGROUND_COLOR):
     pygame.draw.line(screen, FOREGROUND_COLOR, (20, 20), (20, 300))
     pygame.draw.line(screen, FOREGROUND_COLOR, (20, 300), (80, 300))
     pygame.draw.line(screen, FOREGROUND_COLOR, (20, 20), (200, 20))
     pygame.draw.line(screen, FOREGROUND_COLOR, (200, 20), (200, 40))
+    display_message(screen, 'Program made by Mattia Peiretti - www.mattiapeiretti.com', FOREGROUND_COLOR, (10, WINDOW_HEIGHT - 50), myfont_small)
+    display_message(screen, '09-2019', FOREGROUND_COLOR, (10, WINDOW_HEIGHT - 30), myfont_small)
 
-
-def main():
-    pygame.display.set_caption('Hang man')
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+def generate_screen(width, height, BACKGROUND_COLOR):
+    screen = pygame.display.set_mode((width, height))
     screen.fill(BACKGROUND_COLOR)
+    return screen
 
-    p = stick_man(screen, FOREGROUND_COLOR)
+def generate_word():
+    word = random.choice(nouns)
+    return word
 
-    errors = 0
-    line1 = ''
-    line2 = 'Please, select a letter: '
-    line3 = 'Number of errors: '
-    word = ''
-    processed_word = '-'
+def generate_censured_word(word):
+    processed_word = word[0] + '-'*(len(word)-2) + word[len(word)-1]
+    return processed_word
 
-    running = True
-    while running:
-
-        for events in pygame.event.get():
-            if events.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(BACKGROUND_COLOR)
-
-        if errors > 0:
-            p.draw_head()
-
-        if errors > 1:
-            p.draw_body()
-
-        if errors > 2:
-            p.draw_left_arm()
-
-        if errors > 3:
-            p.draw_right_arm()
-
-        if errors > 4:
-            p.draw_left_leg()
-
-        if errors > 5:
-            p.draw_right_leg()
-            pygame.display.flip()
-            sleep(2)
-            show_message_box(screen, 'GAME OVER!!', 3)
-            errors = 0
-
-        else:
-            if processed_word.find('-') == -1:
-                show_message_box(screen, random.choice(compliments), 3)
-                word = ''
-                errors = 0
-
-            if word == '':
-                word = generate_word()
-                processed_word = word[0] + '-'*(len(word)-2) + word[len(word)-1]
-
-            line1 = processed_word
-
-            display_message(screen, line1, FOREGROUND_COLOR, (50, 320))
-            display_message(screen, line2, FOREGROUND_COLOR, (50, 350))
-            display_message(screen, line3 + str(errors), FOREGROUND_COLOR, (50, 380))
-
-            line2 = 'Please, select a letter: '
-            draw_base(screen, FOREGROUND_COLOR)
-            pygame.display.flip()
-
-            letter = ask_letter(screen, 80, 350)
-            if letter == '':
-                show_message_box(screen, 'Please, insert only letters!', 2)
-            line2 = 'You selected the letter: ' + letter
-            print(word)
-            word = list(word)
-            word[len(word)-1] = '1'
-            word[0] = '1'
-
-            if letter in word:
-                letter_pos = [i for i, a in enumerate(word) if a == letter]
-                processed_word = list(processed_word)
-                for key in letter_pos:
-                    processed_word[key] = letter
-                    word[key] = '1'
-                processed_word = ''.join(processed_word)
-                line1 = processed_word
-
-            else:
-                errors += 1
-
-
-def display_message(screen, message, color, pos=(50, 30)):
-    text = myfont.render(message, False, color)
+def display_message(screen, message, color, pos=(50, 30), font = myfont):
+    text = font.render(message, False, color)
     screen.blit(text, pos)
 
-
-def show_message_box(screen, message, showtime):
-    screen.fill((0, 0, 0))
+def show_message_box(screen, message, showtime): #showing a messagebox, like a notification for the user
+    screen.fill((0, 0, 0)) #filling screen black
     text = myfont_big.render(message, False, (255, 255, 255))
     text_rect = text.get_rect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
     screen.blit(text, text_rect)
@@ -162,36 +88,113 @@ def show_message_box(screen, message, showtime):
     screen.fill((0, 0, 0))
     pygame.display.flip()
 
-
-def detect_pressed_key(screen, key_pressed_ascii):
-    key_pressed = ""
-    if key_pressed_ascii >= MIN_ASCII_VALUE and key_pressed_ascii <= MAX_ASCII_VALUE:
-        key_pressed = chr(key_pressed_ascii)
-        if key_pressed == "":
-            pass
-        else:
-            return key_pressed
+def get_char_from_ascii(screen, key_pressed_ascii): #converting ascii code to char var
+    if key_pressed_ascii >= MIN_ASCII_VALUE and key_pressed_ascii <= MAX_ASCII_VALUE: #checking if it is a letter
+        key_pressed = chr(key_pressed_ascii) #converting ascii num to char
+        if not key_pressed == "":
+            return key_pressed #returning letter as a string
     else:
+        return '' #return a empty str if the pressed key is not a letter, but maybe a special char, like !@#$%^&* etc...
 
-        return key_pressed
-
-
-def ask_letter(screen, cord_x=50, cord_y=50):
+def ask_letter(screen, cord_x=50, cord_y=50): #waiting for user to input a letter
     running = True
     while running:
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 sys.exit()
-            if events.type == pygame.KEYDOWN:
-                letter = detect_pressed_key(screen, events.key)
-                running = False
+            if events.type == pygame.KEYDOWN: #if a key gets pressed
+                letter = get_char_from_ascii(screen, events.key) #returns letter from ascii value
+                running = False #killing loop
     return letter
 
 
-def generate_word():
-    word = random.choice(nouns)
-    return word
 
+
+
+def main():
+    screen = generate_screen(WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND_COLOR) #Init the screen 
+    person = stick_man(screen, FOREGROUND_COLOR) #Init person class
+
+    #Just some vars
+    errors = 0
+    line1 = ''
+    line2 = 'Please, select a letter: '
+    line3 = 'Number of errors: '
+    word = ''
+    processed_word = '-'
+
+    running = True
+    while running: #loop...
+        #checking for events during the loop, this prevents the window from freezing
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                sys.exit() #killing program on close event
+
+        if errors > 5: #checking if the max number of errors has been reached
+            person.draw_right_leg() #drawing the last piece of the person
+            pygame.display.flip() #updating the screen
+            sleep(2) #delay of 2 sec to show the complete drawing of the person
+            show_message_box(screen, 'GAME OVER!!', 3) #showing the gameover screen
+            #init the two vars again \/
+            word = ''
+            errors = 0
+
+        else: #if the max number of errors has not been reached, then
+            if processed_word.find('-') == -1: #checking if the word still contains slots that need to be filled
+                show_message_box(screen, random.choice(compliments), 3) #displaying a compliment to indicate that the user won
+                word = ''
+                errors = 0
+
+            if word == '': #checking if word is not init
+                word = generate_word() #generating a new random word
+                processed_word = generate_censured_word(word) #generating a copy of the word with - instead of the letter in the center
+
+            line1 = processed_word
+
+            #displaying the 3 lines of text
+            display_message(screen, line1, FOREGROUND_COLOR, (50, 320)) #word to guess
+            display_message(screen, line2, FOREGROUND_COLOR, (50, 350)) #selected letter
+            display_message(screen, line3 + str(errors), FOREGROUND_COLOR, (50, 380)) #num of errors
+
+            line2 = 'Please, select a letter: '
+            draw_base(screen, FOREGROUND_COLOR)
+            pygame.display.flip() #updating the screen
+            screen.fill(BACKGROUND_COLOR) #filling the screen black
+
+            letter = ask_letter(screen, 80, 350) #asking for input
+            if letter == '':
+                show_message_box(screen, 'Please, insert only letters!', 2) #checking if the input consists of only letters
+            line2 = 'You selected the letter: ' + letter
+            print('[DEBUG] The word is: ' + ''.join(word))
+            word = list(word) #changing word from str to list, so that every letter is accesible individually...
+            word[len(word)-1] = word[0] = '1' # changing the first and last letter of the word to 1 so that even if the user input those letter, they will be counted as wrong
+
+            if letter in word: #checking if the letter selected is in word
+                letter_pos = [i for i, a in enumerate(word) if a == letter] #getting the posisiton of the letter(s) present in the word
+                processed_word = list(processed_word) #changing processed_word from str to list, so that every letter is accesible individually...
+                for key in letter_pos: #adding the selected letter to processed_word 
+                    processed_word[key] = letter 
+                    word[key] = '1' #setting the seleted letter to 1 in word so that the statement does not accept is as true if input a second time
+                processed_word = ''.join(processed_word) #converting the word again into a str
+                line1 = processed_word #setting processed_word to be displayed
+
+            else:
+                errors += 1 #incrementing errors if the selected letter is wrong
+
+                if errors > 0:
+                    person.draw_head()
+
+                if errors > 1:
+                    person.draw_body()
+
+                if errors > 2:
+                    person.draw_left_arm()
+
+                if errors > 3:
+                    person.draw_right_arm()
+
+                if errors > 4:
+                    person.draw_left_leg()
 
 if __name__ == '__main__':
     main()
